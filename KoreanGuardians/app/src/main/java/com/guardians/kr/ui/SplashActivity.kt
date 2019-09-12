a4cd2664568737e39a7e0ef12af4761038af48b9
sovovy <sovovy@gmail.com>
2019-09-12 18:21:18 +0900

    <expired token - 로그인> 연결

--- KoreanGuardians/app/src/main/java/com/guardians/kr/ui/SplashActivity.kt
+++ KoreanGuardians/app/src/main/java/com/guardians/kr/ui/SplashActivity.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
 import com.guardians.kr.R
+import com.guardians.kr.network.ApplicationController
+import com.guardians.kr.network.NetworkService
+import com.guardians.kr.network.get.GetItemResponse
 import com.guardians.kr.ui.login.LoginActivity
 import com.guardians.kr.ui.main.MainActivity
 import com.guardians.kr.util.SharedPreferenceController
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : AppCompatActivity() {
+    private var networkService : NetworkService = ApplicationController.instance.networkService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +39,22 @@ class SplashActivity : AppCompatActivity() {
         val handler = Handler()
         handler.postDelayed({
             if(SharedPreferenceController.instance.getToken(this)!=""){
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                val getItem = networkService.getItem(SharedPreferenceController.instance.getToken(this), 1, 0)
+                getItem.enqueue(object : Callback<GetItemResponse> {
+                    override fun onFailure(call: Call<GetItemResponse>?, t: Throwable?) {
+                        Log.d("Error::Splash", "$t")
+                    }
+
+                    override fun onResponse(call: Call<GetItemResponse>?, response: Response<GetItemResponse>?) {
+                        if (response!!.message().length >= 8) {
+                            startActivity(Intent(applicationContext, LoginActivity::class.java))
+                            finish()
+                        }else{
+                            startActivity(Intent(applicationContext, MainActivity::class.java))
+                            finish()
+                        }
+                    }
+                })
             } else {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
